6983707cf14bd71c52f2bb1fdaff8d1ed1e8ec6d
Pooja Shah <shahpooja075@gmail.com>
2018-06-16 17:27:16 +0530

    component part 2.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
